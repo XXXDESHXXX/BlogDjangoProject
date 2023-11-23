349420bb84b4f1f4a3c914480a8cba7f1d9bf03e
XXXDESH <kirya.shvarts@list.ru>
2023-11-23 19:20:38 +0300

    Add functions post_detail, post_list, get_queryset

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 # Create your models here.
 class Post(models.Model):
 
@@ -22,12 +27,11 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
-        indexes = [
-            models.Index(fields=['-publish']),
-        ]
 
     def __str__(self):
         return self.title
