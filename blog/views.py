a9bb60caaefe420d0d76ebdbd5d44ec7a841a734
XXXDESHXXX <kirya.shvarts@list.ru>
2023-11-29 12:14:52 +0300

    Create class PostListView; Create function post_share

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.views.generic import ListView
+from .forms import EmailPostForm
 
 
-def post_detail(request, year, month, day, post):
+def post_share(request, post_id):
     post = get_object_or_404(Post,
-                             status=Post.Status.PUBLISHED,
-                             slug=post,
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
 
-    return render(request,
-                  'blog/post/detail.html',
-                  {'post': post}
-                  )
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+        else:
+            form = EmailPostForm()
+        return render(request,
+                      'blog/post/share.html',
+                      {'post': post, 'form': form})
 
 
 # Create your views here.
-def post_list(request):
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
