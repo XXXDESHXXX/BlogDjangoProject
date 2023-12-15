16bb74c4115ca653701752096698bdce3b4102ad
XXXDESHXXX <kirya.shvarts@list.ru>
2023-12-15 12:12:10 +0300

    Add search system; Create class SearchForm; Create function post_search;

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,13 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 
 
 def post_list(request, tag_slug=None):
@@ -43,6 +45,27 @@ def post_detail(request, year, month, day, post):
         'blog/post/detail.html',
         {'post': post, 'comments': comments, 'form': form, 'similar_posts': similar_posts}
     )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form, 'query': query, 'results': results}
+                  )
 
 
 def post_share(request, post_id):
